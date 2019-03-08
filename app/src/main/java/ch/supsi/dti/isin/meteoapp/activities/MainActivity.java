5ad7c92cc1040ac3bc5b0be7df831c0f6dfd7748
Enrico Bottani <enrico.bottani@hotmail.com>
2019-03-08 14:59:18 +0100

    Geolocation permission

--- app/src/main/java/ch/supsi/dti/isin/meteoapp/activities/MainActivity.java
+++ app/src/main/java/ch/supsi/dti/isin/meteoapp/activities/MainActivity.java
@@ -1,7 +1,11 @@
 package ch.supsi.dti.isin.meteoapp.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Geocoder;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 
 import java.util.Locale;
 
@@ -10,9 +14,27 @@ import ch.supsi.dti.isin.meteoapp.fragments.ListFragment;
 public class MainActivity extends SingleFragmentActivity {
 
     public static MainActivity instance;
+
+    static final int GEOLOCATION_PERMISSION = 0;
+
     @Override
     protected Fragment createFragment() {
         instance = this;
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, GEOLOCATION_PERMISSION);
+        }
         return new ListFragment();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case GEOLOCATION_PERMISSION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // ho ottenuto i permessi
+                }
+                return;
+            }
+        }
     }
 }
