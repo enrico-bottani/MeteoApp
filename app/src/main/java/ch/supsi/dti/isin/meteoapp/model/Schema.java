4e48ca46a9d6759976bcf79d753d5d228fb0c0f2
Alice <alice.nesurini@gmail.com>
2019-03-15 14:45:09 +0100

    Fix fragment location (nimisha) + first db schema

---
+++ app/src/main/java/ch/supsi/dti/isin/meteoapp/model/Schema.java
@@ -0,0 +1,11 @@
+package ch.supsi.dti.isin.meteoapp.model;
+
+public class Schema {
+    public static final class Location{
+        public static final String NAME = "location";
+
+        public static final class Columns{
+            public static final String NAME="name";
+        }
+    }
+}
